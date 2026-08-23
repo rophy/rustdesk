886b55431a02817255574f8cfb6b62cd4a64a5fd
Rophy Tsai <rophy@users.noreply.github.com>
2026-08-23 07:48:17 +0000

    fix: add try/catch to opus worker to prevent silent crashes

--- docker/web-deps/opus-worker-glue.js
+++ docker/web-deps/opus-worker-glue.js
@@ -1,12 +1,16 @@
 
 var dec;
 self.addEventListener('message', (e) => {
-  if (e.data.channels > 0) {
-    if (dec) dec.destroy();
-    dec = new Decoder(e.data.channels, e.data.sampleRate);
-  } else {
-    if (!dec) return;
-    dec.input(e.data);
-    self.postMessage(dec.output().slice(0));
+  try {
+    if (e.data.channels > 0) {
+      if (dec) dec.destroy();
+      dec = new Decoder(e.data.channels, e.data.sampleRate);
+    } else {
+      if (!dec) return;
+      dec.input(e.data);
+      self.postMessage(dec.output().slice(0));
+    }
+  } catch (err) {
+    console.error('opus worker:', err);
   }
 });
